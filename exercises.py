34d9ba87bd404d803a6a3a1e0b45f7e7fac12da8
jadenpok <jaden.pok12@gmail.com>
2026-04-25 16:05:46 +0000

    Finished exercises

--- exercises.py
+++ exercises.py
@@ -21,8 +21,10 @@ def calculate_average(times: list[float]) -> float:
     Hint: Use a for loop to add up all values, then divide by len(times)
     """
     # Your code here
-
-    return 0
+    sum=0
+    for t in times:
+        sum=sum+t
+    return(sum/len(times))
 
 
 def get_fastest_time(times: list[float]) -> float:
@@ -41,8 +43,11 @@ def get_fastest_time(times: list[float]) -> float:
     Hint: Use a for loop to check each time and keep track of the smallest one
     """
     # Your code here
-
-    return 0
+    fastest_time=100000000
+    for t in times:
+        if fastest_time > t:
+            fastest_time=t
+    return(fastest_time)
 
 
 def get_slowest_time(times: list[float]) -> float:
@@ -61,8 +66,11 @@ def get_slowest_time(times: list[float]) -> float:
     Hint: Use a for loop to check each time and keep track of the largest one
     """
     # Your code here
-
-    return 0
+    slowest_time=1
+    for t in times:
+        if slowest_time < t:
+            slowest_time=t
+    return(slowest_time)
 
 
 def get_description(average: float) -> str:
@@ -85,5 +93,21 @@ def get_description(average: float) -> str:
         - 350ms or more: "Need a bit of practice, but you have potential."
     """
     # Your code here
+    def get_description(time):
+        if average <180:
+            return("elite")
+        elif average <200:
+            return("lightning")
+        elif average <225:
+            return("impressive")
+        elif average <275:
+            return("nice")
+        elif average <300:
+            return("good")
+        elif average <350:
+            return("shabby")
+        elif average <100000000000000:
+            return("more practice")
+        
 
     return "Complete the Python exercises to see your results!"
